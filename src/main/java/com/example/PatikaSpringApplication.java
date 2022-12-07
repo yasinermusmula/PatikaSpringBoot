10a01441f2a9d50a0a3ac2491baf17422f08efaa
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2022-12-07 16:56:27 +0300

    first commit

---
+++ src/main/java/com/example/PatikaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatikaSpringApplication.class, args);
+    }
+
+}
